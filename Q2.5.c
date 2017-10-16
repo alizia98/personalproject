d2ad0f324f2d541be0833fa3355da1f90884d381
alizia98 <31632250+alizia98@users.noreply.github.com>
2017-10-16 13:34:40 +0100

    Add files via upload

---
+++ Q2.5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main ()
+{
+    int z = 1;
+    int y = 6; 
+    
+    
+    for (z=1;z<7;z++)
+    {      
+        
+        
+        int x = 1;
+        for (x=1;x<y;x++)
+        {
+            printf("*");
+                    
+        }
+        printf("\n");   
+        y = y -1;
+        
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
